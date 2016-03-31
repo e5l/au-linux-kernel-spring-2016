5095cffcc88ece369161853128bf7be6c55be9be
e5l <leonid.stashevsky@gmail.com>
2016-03-31 17:25:55 +0300

    cw

--- tasks/virt_mem_max/main.c
+++ tasks/virt_mem_max/main.c
@@ -1,5 +1,31 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
 int main()
 {
-    // TODO
+    size_t size = -1;
+    size_t result = 0;
+
+    int fd = open("/dev/zero", O_RDONLY);
+
+    while (size != 0)
+    {
+        int64_t i = (int64_t) mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+        if (i != -1)
+            result += size;
+
+        size /= 2;
+    }
+
+    size = -1;
+
+    printf("%zu\n", result);
     return 0;
 }
